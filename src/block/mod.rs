0c2670ee01be94c4354689c0e155a42bcf379851
Junha <vjunhav@gmail.com>
2024-07-17 00:35:28 +0900

    Add SCALE Encode impls for Extrinsics and Block

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -1,2 +1,21 @@
+use crate::block::{extrinsics::Extrinsics, header::BlockHeader};
+use parity_scale_codec::{Encode, Output};
+
 mod extrinsics;
 mod header;
+
+pub struct Block {
+    header: BlockHeader,
+    extrinsics: Extrinsics,
+}
+
+impl Encode for Block {
+    fn size_hint(&self) -> usize {
+        self.header.size_hint() + self.extrinsics.size_hint()
+    }
+
+    fn encode_to<W: Output + ?Sized>(&self, dest: &mut W) {
+        self.header.encode_to(dest);
+        self.extrinsics.encode_to(dest);
+    }
+}
