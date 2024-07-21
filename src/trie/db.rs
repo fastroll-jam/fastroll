430b5aefae1e97eb46d14e10c76c05083ae964e0
Junha <vjunhav@gmail.com>
2024-07-21 16:53:02 +0900

    Add `GlobalStateManager` with singleton lazy initialization which will work as a unified interface for the KVDB (RocksDB) and different state components

--- src/trie/db.rs
+++ src/trie/db.rs
@@ -1,5 +1,10 @@
+use dotenv::dotenv;
+use lazy_static::lazy_static;
 use rocksdb::{Options, DB as RocksDB};
-use std::sync::Arc;
+use std::{
+    env,
+    sync::{Arc, Mutex},
+};
 
 // Key-Value database representation
 
@@ -7,6 +12,26 @@ use std::sync::Arc;
 pub(crate) trait KeyValueDB: Sync + Send {
     fn put(&self, key: &[u8], value: &[u8]) -> Result<(), String>;
     fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, String>;
+}
+
+struct DB {
+    inner: Arc<dyn KeyValueDB>,
+}
+
+impl DB {
+    fn new(inner: Arc<dyn KeyValueDB>) -> Self {
+        DB { inner }
+    }
+}
+
+impl KeyValueDB for DB {
+    fn put(&self, key: &[u8], value: &[u8]) -> Result<(), String> {
+        self.inner.put(key, value)
+    }
+
+    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, String> {
+        self.inner.get(key)
+    }
 }
 
 // Wrapper around RocksDB to implement KeyValueDB
@@ -33,22 +58,31 @@ impl KeyValueDB for RocksDBWrapper {
     }
 }
 
-struct DB {
-    inner: Arc<dyn KeyValueDB>,
+// Global state manager for the KVDB instance
+pub(crate) struct GlobalStateManager {
+    db: Arc<dyn KeyValueDB>,
 }
 
-impl DB {
-    fn new(inner: Arc<dyn KeyValueDB>) -> Self {
-        DB { inner }
+impl GlobalStateManager {
+    fn new(db: Arc<dyn KeyValueDB>) -> Self {
+        GlobalStateManager { db }
     }
+
+    fn get_db(&self) -> Arc<dyn KeyValueDB> {
+        Arc::clone(&self.db)
+    }
+
+    // TODO: state retrieval / update functions here
 }
 
-impl KeyValueDB for DB {
-    fn put(&self, key: &[u8], value: &[u8]) -> Result<(), String> {
-        self.inner.put(key, value)
-    }
+// Lazy initialization for a Singleton access to the DB instance
+lazy_static! {
+    pub(crate) static ref GLOBAL_STATE_MANAGER: Mutex<GlobalStateManager> = {
+        dotenv().ok();
+        let db_path = env::var("ROCKSDB_PATH").expect("ROCKSDB_PATH must be set correctly");
 
-    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, String> {
-        self.inner.get(key)
-    }
+        Mutex::new(GlobalStateManager::new(Arc::new(RocksDBWrapper::new(
+            &db_path,
+        ))))
+    };
 }
