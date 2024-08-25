d896afa30596c6111f8f878ddb71b85b6af11975
Junha Park <vjunhav@gmail.com>
2024-08-25 22:51:27 +0900

    Add PVM basic structures

---
+++ pvm/src/constants.rs
@@ -0,0 +1,5 @@
+pub const REGISTERS_COUNT: usize = 13;
+pub const MEMORY_SIZE: usize = 1 << 32;
+pub const PAGE_SIZE: usize = 1 << 14; // Z_P
+pub const SEGMENT_SIZE: usize = 1 << 16; // Z_Q
+pub const INPUT_SIZE: usize = 1 << 24; // Z_I
