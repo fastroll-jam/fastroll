908e9602d4260b1006e84a77c89bb99a2985087b
Junha Park <vjunhav@gmail.com>
2024-08-27 00:26:10 +0900

    Add PVM layout with the standard program initialization function and the common invocation function (WIP)

--- pvm/src/constants.rs
+++ pvm/src/constants.rs
@@ -1,5 +1,6 @@
 pub const REGISTERS_COUNT: usize = 13;
-pub const MEMORY_SIZE: usize = 1 << 32;
-pub const PAGE_SIZE: usize = 1 << 14; // Z_P
+pub const MEMORY_SIZE: usize = u32::MAX as usize;
+pub const PAGE_SIZE: usize = 1 << 14; // Z_P; 16KB page size
 pub const SEGMENT_SIZE: usize = 1 << 16; // Z_Q
 pub const INPUT_SIZE: usize = 1 << 24; // Z_I
+pub const STANDARD_PROGRAM_SIZE_LIMIT: usize = u32::MAX as usize;
