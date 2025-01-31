7bcfcab101a3fc4d2b3aabe8cb8ae87e6926e620
Junha Park <0xjunha@gmail.com>
2025-01-31 16:37:41 +0900

    Add PVM test case parser

--- conformance-tests/src/pvm_harness.rs
+++ conformance-tests/src/pvm_harness.rs
@@ -1,9 +1,18 @@
+use rjam_common::UnsignedGas;
+use rjam_pvm_core::{
+    constants::{MEMORY_SIZE, PAGE_SIZE},
+    core::VMState,
+    state::{
+        memory::{AccessType, Memory},
+        register::Register,
+    },
+    types::common::RegValue,
+};
 use serde::{Deserialize, Serialize};
 use std::{
     fs,
     path::{Path, PathBuf},
 };
-
 // --- Types
 
 #[allow(non_camel_case_types)]
@@ -20,16 +29,38 @@ pub enum ExpectedStatus {
 
 /// A blob of bytes at a given memory address.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-pub struct MemoryChunk {
+struct MemoryChunk {
     pub address: u32,
     pub contents: Vec<u8>,
 }
 
+impl MemoryChunk {
+    fn write_to_memory(&self, memory: &mut Memory) {
+        memory
+            .write_bytes(self.address, &self.contents)
+            .expect("Failed to write memory");
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-pub struct PageMap {
+struct PageMap {
     pub address: u32,
     pub length: u32,
     pub is_writable: bool,
+}
+
+impl PageMap {
+    fn set_access(&self, memory: &mut Memory) {
+        let access_type = if self.is_writable {
+            AccessType::ReadWrite
+        } else {
+            AccessType::ReadOnly
+        };
+
+        memory
+            .set_address_range_access(self.address..(self.address + self.length), access_type)
+            .expect("Failed to set access");
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
@@ -45,7 +76,7 @@ pub struct TestCase {
     /// any address not on this should be inaccessible.
     pub initial_page_map: Vec<PageMap>,
     /// Lists all non-zero values to put in memory before execution.
-    pub initial_memory: MemoryChunk,
+    pub initial_memory: Vec<MemoryChunk>,
     /// The initial amount of gas.
     pub initial_gas: i64,
     /// The program blob to be executed as part of the test.
@@ -58,20 +89,84 @@ pub struct TestCase {
     pub expected_pc: u32,
     /// Lists all non-zero values after the execution finishes; all accessible addresses not on this
     /// must be filled with zeroes.
-    pub expected_memory: MemoryChunk,
+    pub expected_memory: Vec<MemoryChunk>,
     /// The final amount of gas remaining after the execution finishes.
     pub expected_gas: i64,
     /// The address of a page fault (only if the program finishes with a page fault).
     pub expected_page_fault_address: Option<u32>,
 }
 
+pub struct ParsedTestCase {
+    initial_vm: VMState,
+    program: Vec<u8>,
+    expected_vm: VMState,
+    expected_status: ExpectedStatus,
+    expected_page_fault_address: Option<u32>,
+}
+
 // --- Test Harness
 
-pub const PATH_PREFIX: &str = "jamtestvectors-pvm/pvm/programs";
+const PATH_PREFIX: &str = "jamtestvectors-pvm/pvm/programs";
 
 pub fn load_test_case(filename: &Path) -> TestCase {
     let path = PathBuf::from(PATH_PREFIX).join(filename);
     let full_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(path);
     let json_str = fs::read_to_string(&full_path).expect("Failed to read test vector file");
     serde_json::from_str(&json_str).expect("Failed to parse JSON")
+}
+
+pub fn parse_test_case(test_case: TestCase) -> ParsedTestCase {
+    // --- Initial VM State
+    let mut initial_vm = VMState {
+        registers: [Register::default(); 13],
+        memory: Memory::new(MEMORY_SIZE, PAGE_SIZE),
+        pc: test_case.initial_pc as RegValue,
+        gas_counter: test_case.initial_gas as UnsignedGas,
+    };
+
+    // Setup registers
+    for (i, val) in test_case.initial_regs.iter().enumerate() {
+        initial_vm.registers[i] = Register::new(*val);
+    }
+
+    // Setup memory
+    for page_map in &test_case.initial_page_map {
+        page_map.set_access(&mut initial_vm.memory);
+    }
+
+    // Load initial data to value
+    for memory_chunk in test_case.initial_memory {
+        memory_chunk.write_to_memory(&mut initial_vm.memory);
+    }
+
+    // --- Expected VM State after program run
+    let mut expected_vm = VMState {
+        registers: [Register::default(); 13],
+        memory: Memory::new(MEMORY_SIZE, PAGE_SIZE),
+        pc: test_case.expected_pc as RegValue,
+        gas_counter: test_case.expected_gas as UnsignedGas,
+    };
+
+    // Setup registers
+    for (i, val) in test_case.expected_regs.iter().enumerate() {
+        expected_vm.registers[i] = Register::new(*val);
+    }
+
+    // Setup memory
+    for page_map in &test_case.initial_page_map {
+        page_map.set_access(&mut expected_vm.memory);
+    }
+
+    // Load initial data to value
+    for memory_chunk in test_case.expected_memory {
+        memory_chunk.write_to_memory(&mut expected_vm.memory);
+    }
+
+    ParsedTestCase {
+        initial_vm,
+        program: test_case.program,
+        expected_vm,
+        expected_status: test_case.expected_status,
+        expected_page_fault_address: test_case.expected_page_fault_address,
+    }
 }
