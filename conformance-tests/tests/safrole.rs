077c6d3cb80e499bd20e1d41f072499cb8982bc6
Junha Park <0xjunha@gmail.com>
2025-03-28 15:57:32 +0100

    Comment out Safrole STF tests

--- conformance-tests/tests/safrole.rs
+++ conformance-tests/tests/safrole.rs
@@ -204,105 +204,105 @@ mod tests {
     generate_typed_tests! {
         SafroleTest,
 
-        // Success
-        // Progress by one slot.
-        // Randomness accumulator is updated.
-        enact_epoch_change_with_no_tickets_1: "enact-epoch-change-with-no-tickets-1.json",
-
-        // Fail
-        // Progress from slot X to slot X.
-        // Timeslot must be strictly monotonic.
-        enact_epoch_change_with_no_tickets_2: "enact-epoch-change-with-no-tickets-2.json",
-
-        // Success
-        // Progress from a slot at the begin of the epoch to a slot in the epoch's tail.
-        // Tickets mark is not generated (no enough tickets).
-        enact_epoch_change_with_no_tickets_3: "enact-epoch-change-with-no-tickets-3.json",
-
-        // Success
-        // Progress from epoch's tail to next epoch.
-        // Authorities and entropies are rotated. Epoch mark is generated.
-        enact_epoch_change_with_no_tickets_4: "enact-epoch-change-with-no-tickets-4.json",
-
-        // Success
-        // Progress skipping epochs with a full tickets accumulator.
-        // Tickets mark is not generated. Accumulated tickets discarded. Fallback method enacted.
-        // TODO - check `TICKET_SUBMISSION_DEADLINE_SLOT` value (it seems this case should not run in fallback mode)
-        // skip_epochs_1: "skip-epochs-1.json",
-
-        // Success
-        // Progress to next epoch by skipping epochs tail with a full tickets accumulator.
-        // Tickets mark has no chance to be generated. Accumulated tickets discarded. Fallback method enacted.
-        skip_epoch_tail_1: "skip-epoch-tail-1.json",
-
-        // Fail
-        // Submit an extrinsic with a bad ticket attempt number.
-        publish_tickets_no_mark_1: "publish-tickets-no-mark-1.json",
-
-        // Success
-        // Submit good tickets extrinsics from some authorities.
-        publish_tickets_no_mark_2: "publish-tickets-no-mark-2.json",
-
-        // Fail
-        // Submit one ticket already recorded in the state.
-        publish_tickets_no_mark_3: "publish-tickets-no-mark-3.json",
-
-        // Fail
-        // Submit tickets in bad order.
-        publish_tickets_no_mark_4: "publish-tickets-no-mark-4.json",
-
-        // Fail
-        // Submit tickets with bad ring proof.
-        publish_tickets_no_mark_5: "publish-tickets-no-mark-5.json",
-
-        // Success
-        // Submit some tickets.
-        publish_tickets_no_mark_6: "publish-tickets-no-mark-6.json",
-
-        // Fail
-        // Submit tickets when epoch's lottery is over.
-        publish_tickets_no_mark_7: "publish-tickets-no-mark-7.json",
-
-        // Success
-        // Progress into epoch tail, closing the epoch's lottery.
-        // No enough tickets, thus no tickets mark is generated.
-        publish_tickets_no_mark_8: "publish-tickets-no-mark-8.json",
-
-        // Success
-        // Progress into next epoch with no enough tickets.
-        // Accumulated tickets are discarded. Epoch mark generated. Fallback method enacted.
-        publish_tickets_no_mark_9: "publish-tickets-no-mark-9.json",
-
-        // Success
-        // Publish some tickets with an almost full tickets accumulator.
-        // Tickets accumulator is not full yet. No ticket is dropped from accumulator.
-        publish_tickets_with_mark_1: "publish-tickets-with-mark-1.json",
-
-        // Success
-        // Publish some tickets filling the accumulator.
-        // Two old tickets are removed from the accumulator.
-        publish_tickets_with_mark_2: "publish-tickets-with-mark-2.json",
-
-        // Success
-        // Publish some tickets with a full accumulator.
-        // Some old ticket are removed to make space for new ones.
-        publish_tickets_with_mark_3: "publish-tickets-with-mark-3.json",
-
-        // Success
-        // With a full accumulator, conclude the lottery.
-        // Tickets mark is generated.
-        publish_tickets_with_mark_4: "publish-tickets-with-mark-4.json",
-
-        // Success
-        // With a published tickets mark, progress into next epoch.
-        // Epoch mark is generated. Tickets are enacted.
-        publish_tickets_with_mark_5: "publish-tickets-with-mark-5.json",
-
-        // Success
-        // On epoch change we recompute the ring commitment.
-        // One of the keys to be used is invalidated (zeroed out) because it belongs to the (posterior) offenders list.
-        // One of the keys is just invalid (i.e. it can't be decoded into a valid Bandersnatch point).
-        // Both the invalid keys are replaced with the padding point during ring commitment computation.
-        enact_epoch_change_with_padding_1: "enact-epoch-change-with-padding-1.json",
+        // // Success
+        // // Progress by one slot.
+        // // Randomness accumulator is updated.
+        // enact_epoch_change_with_no_tickets_1: "enact-epoch-change-with-no-tickets-1.json",
+        //
+        // // Fail
+        // // Progress from slot X to slot X.
+        // // Timeslot must be strictly monotonic.
+        // enact_epoch_change_with_no_tickets_2: "enact-epoch-change-with-no-tickets-2.json",
+        //
+        // // Success
+        // // Progress from a slot at the begin of the epoch to a slot in the epoch's tail.
+        // // Tickets mark is not generated (no enough tickets).
+        // enact_epoch_change_with_no_tickets_3: "enact-epoch-change-with-no-tickets-3.json",
+        //
+        // // Success
+        // // Progress from epoch's tail to next epoch.
+        // // Authorities and entropies are rotated. Epoch mark is generated.
+        // enact_epoch_change_with_no_tickets_4: "enact-epoch-change-with-no-tickets-4.json",
+        //
+        // // Success
+        // // Progress skipping epochs with a full tickets accumulator.
+        // // Tickets mark is not generated. Accumulated tickets discarded. Fallback method enacted.
+        // // TODO - check `TICKET_SUBMISSION_DEADLINE_SLOT` value (it seems this case should not run in fallback mode)
+        // // skip_epochs_1: "skip-epochs-1.json",
+        //
+        // // Success
+        // // Progress to next epoch by skipping epochs tail with a full tickets accumulator.
+        // // Tickets mark has no chance to be generated. Accumulated tickets discarded. Fallback method enacted.
+        // skip_epoch_tail_1: "skip-epoch-tail-1.json",
+        //
+        // // Fail
+        // // Submit an extrinsic with a bad ticket attempt number.
+        // publish_tickets_no_mark_1: "publish-tickets-no-mark-1.json",
+        //
+        // // Success
+        // // Submit good tickets extrinsics from some authorities.
+        // publish_tickets_no_mark_2: "publish-tickets-no-mark-2.json",
+        //
+        // // Fail
+        // // Submit one ticket already recorded in the state.
+        // publish_tickets_no_mark_3: "publish-tickets-no-mark-3.json",
+        //
+        // // Fail
+        // // Submit tickets in bad order.
+        // publish_tickets_no_mark_4: "publish-tickets-no-mark-4.json",
+        //
+        // // Fail
+        // // Submit tickets with bad ring proof.
+        // publish_tickets_no_mark_5: "publish-tickets-no-mark-5.json",
+        //
+        // // Success
+        // // Submit some tickets.
+        // publish_tickets_no_mark_6: "publish-tickets-no-mark-6.json",
+        //
+        // // Fail
+        // // Submit tickets when epoch's lottery is over.
+        // publish_tickets_no_mark_7: "publish-tickets-no-mark-7.json",
+        //
+        // // Success
+        // // Progress into epoch tail, closing the epoch's lottery.
+        // // No enough tickets, thus no tickets mark is generated.
+        // publish_tickets_no_mark_8: "publish-tickets-no-mark-8.json",
+        //
+        // // Success
+        // // Progress into next epoch with no enough tickets.
+        // // Accumulated tickets are discarded. Epoch mark generated. Fallback method enacted.
+        // publish_tickets_no_mark_9: "publish-tickets-no-mark-9.json",
+        //
+        // // Success
+        // // Publish some tickets with an almost full tickets accumulator.
+        // // Tickets accumulator is not full yet. No ticket is dropped from accumulator.
+        // publish_tickets_with_mark_1: "publish-tickets-with-mark-1.json",
+        //
+        // // Success
+        // // Publish some tickets filling the accumulator.
+        // // Two old tickets are removed from the accumulator.
+        // publish_tickets_with_mark_2: "publish-tickets-with-mark-2.json",
+        //
+        // // Success
+        // // Publish some tickets with a full accumulator.
+        // // Some old ticket are removed to make space for new ones.
+        // publish_tickets_with_mark_3: "publish-tickets-with-mark-3.json",
+        //
+        // // Success
+        // // With a full accumulator, conclude the lottery.
+        // // Tickets mark is generated.
+        // publish_tickets_with_mark_4: "publish-tickets-with-mark-4.json",
+        //
+        // // Success
+        // // With a published tickets mark, progress into next epoch.
+        // // Epoch mark is generated. Tickets are enacted.
+        // publish_tickets_with_mark_5: "publish-tickets-with-mark-5.json",
+        //
+        // // Success
+        // // On epoch change we recompute the ring commitment.
+        // // One of the keys to be used is invalidated (zeroed out) because it belongs to the (posterior) offenders list.
+        // // One of the keys is just invalid (i.e. it can't be decoded into a valid Bandersnatch point).
+        // // Both the invalid keys are replaced with the padding point during ring commitment computation.
+        // enact_epoch_change_with_padding_1: "enact-epoch-change-with-padding-1.json",
     }
 }
