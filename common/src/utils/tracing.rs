a0582fa82111ba2681076b61be914c500a3793f9
Junha Park <0xjunha@gmail.com>
2025-04-22 14:24:26 +0900

    [Common] Update tracing setup
    * Added a setup function with a default timer
    * Globally initialize Once

--- common/src/utils/tracing.rs
+++ common/src/utils/tracing.rs
@@ -1,12 +1,27 @@
+use std::sync::Once;
 use tracing::subscriber::set_global_default;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter, Registry};
 
 pub fn setup_timed_tracing() {
-    let fmt_layer = fmt::layer()
-        .with_target(false)
-        .with_timer(fmt::time::uptime());
-    let sub = Registry::default()
-        .with(EnvFilter::from_default_env())
-        .with(fmt_layer);
-    set_global_default(sub).expect("Failed to set tracing subscriber");
+    static INIT_TIMED_TRACING: Once = Once::new();
+    INIT_TIMED_TRACING.call_once(|| {
+        let fmt_layer = fmt::layer()
+            .with_target(false)
+            .with_timer(fmt::time::uptime());
+        let sub = Registry::default()
+            .with(EnvFilter::from_default_env())
+            .with(fmt_layer);
+        set_global_default(sub).expect("Failed to set tracing subscriber");
+    });
+}
+
+pub fn setup_tracing() {
+    static INIT_TRACING: Once = Once::new();
+    INIT_TRACING.call_once(|| {
+        let fmt_layer = fmt::layer().with_target(false);
+        let sub = Registry::default()
+            .with(EnvFilter::from_default_env())
+            .with(fmt_layer);
+        set_global_default(sub).expect("Failed to set tracing subscriber");
+    })
 }
