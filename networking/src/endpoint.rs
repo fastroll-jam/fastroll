9767f404b529bcb57615959327c5057f9526268b
Junha Park <0xjunha@gmail.com>
2025-04-30 19:04:24 +0900

    Update `JamNode` type

---
+++ networking/src/endpoint.rs
@@ -0,0 +1,4 @@
+#[allow(dead_code)]
+pub struct QuicEndpoint {
+    inner: quinn::Endpoint,
+}
--- node/src/node.rs
+++ node/src/node.rs
@@ -1,12 +1,13 @@
 use rjam_block::header_db::BlockHeaderDB;
 use rjam_crypto::types::ValidatorKey;
+use rjam_networking::endpoint::QuicEndpoint;
 use rjam_state::manager::StateManager;
 use std::{net::SocketAddrV6, sync::Arc};
 
 #[allow(dead_code)]
-struct ValidatorInfo {
-    socket_addr_v6: SocketAddrV6,
-    validator_key: ValidatorKey,
+pub struct ValidatorInfo {
+    pub socket_addr_v6: SocketAddrV6,
+    pub validator_key: ValidatorKey,
 }
 
 impl ValidatorInfo {
@@ -19,9 +20,25 @@ impl ValidatorInfo {
     }
 }
 
-#[allow(dead_code)]
 pub struct JamNode {
-    validator_info: ValidatorInfo,
-    state_manager: Arc<StateManager>,
-    header_db: Arc<BlockHeaderDB>,
+    pub validator_info: ValidatorInfo,
+    pub state_manager: Arc<StateManager>,
+    pub header_db: Arc<BlockHeaderDB>,
+    pub endpoint: QuicEndpoint,
+}
+
+impl JamNode {
+    pub fn new(
+        validator_info: ValidatorInfo,
+        state_manager: Arc<StateManager>,
+        header_db: Arc<BlockHeaderDB>,
+        endpoint: QuicEndpoint,
+    ) -> Self {
+        Self {
+            validator_info,
+            state_manager,
+            header_db,
+            endpoint,
+        }
+    }
 }
