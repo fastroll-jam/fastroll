969663cbd9378a8d65a42234e5c7780c46ac958d
Junha Park <0xjunha@gmail.com>
2025-05-03 10:37:14 +0900

    Predict post-STF state: ActiveSet & PastSet

--- transition/src/state_prediction/mod.rs
+++ transition/src/state_prediction/mod.rs
@@ -3,6 +3,7 @@ use rjam_state::types::Timeslot;
 
 pub mod entropy;
 pub mod slot_sealers;
+pub mod validator_sets;
 
 pub(crate) fn epoch_progressed(prior_timeslot: &Timeslot, new_timeslot: &Timeslot) -> bool {
     prior_timeslot.epoch() < new_timeslot.epoch()
---
+++ transition/src/state_prediction/validator_sets.rs
@@ -0,0 +1,35 @@
+use crate::state_prediction::epoch_progressed;
+use rjam_state::{
+    error::StateManagerError,
+    manager::StateManager,
+    types::{ActiveSet, PastSet, Timeslot},
+};
+use std::sync::Arc;
+
+pub async fn predict_active_set(
+    state_manager: Arc<StateManager>,
+    header_timeslot_index: u32,
+) -> Result<ActiveSet, StateManagerError> {
+    let prior_timeslot = state_manager.get_timeslot_clean().await?;
+    let new_timeslot = Timeslot::new(header_timeslot_index);
+    if epoch_progressed(&prior_timeslot, &new_timeslot) {
+        Ok(ActiveSet(
+            state_manager.get_safrole_clean().await?.pending_set,
+        ))
+    } else {
+        state_manager.get_active_set_clean().await
+    }
+}
+
+pub async fn predict_past_set(
+    state_manager: Arc<StateManager>,
+    header_timeslot_index: u32,
+) -> Result<PastSet, StateManagerError> {
+    let prior_timeslot = state_manager.get_timeslot_clean().await?;
+    let new_timeslot = Timeslot::new(header_timeslot_index);
+    if epoch_progressed(&prior_timeslot, &new_timeslot) {
+        Ok(PastSet(state_manager.get_active_set_clean().await?.0))
+    } else {
+        state_manager.get_past_set_clean().await
+    }
+}
