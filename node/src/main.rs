a75e1e6fc4fcdd9a1e234a063a180636617f3a68
Junha Park <0xjunha@gmail.com>
2025-05-11 00:34:16 +0100

    Update main function

--- node/src/main.rs
+++ node/src/main.rs
@@ -3,15 +3,13 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let _node = init_node().await?;
+    let node = init_node().await?;
 
     // Bind to a socket address and start accepting connections
-    // let mut network_manager = node.network_manager.clone();
-    // tokio::spawn(async move { network_manager.run_as_server().await });
+    let network_manager = node.network_manager.clone();
+    network_manager.run_as_server().await?;
 
-    // Connect to peers
-
-    // TODO: Timeslot scheduling, tickets submission
+    // TODO: Connect to peers, timeslot scheduling, tickets submission
 
     Ok(())
 }
