0631f2df6d900f4db1944d9d7e29cd568c7c6b25
Junha Park <0xjunha@gmail.com>
2025-05-12 17:33:23 +0100

    Add conversion methods between stream kind enum and u8 values

--- network/src/error.rs
+++ network/src/error.rs
@@ -1,5 +1,5 @@
 use fr_state::error::StateManagerError;
-use quinn::{ConnectError, ConnectionError, WriteError};
+use quinn::{ConnectError, ConnectionError, ReadToEndError, WriteError};
 use thiserror::Error;
 use tokio::task::JoinError;
 
@@ -13,12 +13,18 @@ pub enum NetworkError {
     ConnectionError(#[from] ConnectionError),
     #[error("quinn WriteError: {0}")]
     WriteError(#[from] WriteError),
+    #[error("quinn ReadToEndError: {0}")]
+    ReadToEndError(#[from] ReadToEndError),
     #[error("tokio JoinError: {0}")]
     JoinError(#[from] JoinError),
     #[error("Invalid local address")]
     InvalidLocalAddr,
     #[error("Invalid peer address format: should be SocketAddrV6")]
     InvalidPeerAddrFormat,
+    #[error("Invalid UP stream kind value: {0}")]
+    InvalidUpStreamKind(u8),
+    #[error("Invalid CE stream kind value: {0}")]
+    InvalidCeStreamKind(u8),
     #[error("The Ed25519 public key is not registered as a network validator peer")]
     ValidatorPeerKeyNotFound,
 }
--- network/src/streams.rs
+++ network/src/streams.rs
@@ -1,3 +1,4 @@
+use crate::error::NetworkError;
 use quinn::{RecvStream, SendStream};
 
 /// Persistent UP stream handles
@@ -16,13 +17,30 @@ pub enum StreamKind {
 }
 
 #[repr(u8)]
-#[derive(Debug, Hash, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub enum UpStreamKind {
     BlockAnnouncement = 0,
 }
 
+impl TryFrom<u8> for UpStreamKind {
+    type Error = NetworkError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(UpStreamKind::BlockAnnouncement),
+            _ => Err(NetworkError::InvalidUpStreamKind(value)),
+        }
+    }
+}
+
+impl UpStreamKind {
+    pub fn from_u8(value: u8) -> Result<Self, NetworkError> {
+        Self::try_from(value)
+    }
+}
+
 #[repr(u8)]
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum CeStreamKind {
     BlockRequest = 128,
     StateRequest = 129,
@@ -41,6 +59,39 @@ pub enum CeStreamKind {
     PreimageRequest = 143,
     AuditAnnouncement = 144,
     JudgmentPublication = 145,
+}
+
+impl TryFrom<u8> for CeStreamKind {
+    type Error = NetworkError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            128 => Ok(CeStreamKind::BlockRequest),
+            129 => Ok(CeStreamKind::StateRequest),
+            131 => Ok(CeStreamKind::TicketDistributionFirst),
+            132 => Ok(CeStreamKind::TicketDistributionSecond),
+            133 => Ok(CeStreamKind::WorkPackageSubmission),
+            134 => Ok(CeStreamKind::WorkPackageSharing),
+            135 => Ok(CeStreamKind::WorkReportDistribution),
+            136 => Ok(CeStreamKind::WorkReportRequest),
+            137 => Ok(CeStreamKind::ShardDistribution),
+            138 => Ok(CeStreamKind::AuditShardRequest),
+            139 => Ok(CeStreamKind::SegmentShardRequestFirst),
+            140 => Ok(CeStreamKind::SegmentShardRequestSecond),
+            141 => Ok(CeStreamKind::AssuranceDistribution),
+            142 => Ok(CeStreamKind::PreimageAnnouncement),
+            143 => Ok(CeStreamKind::PreimageRequest),
+            144 => Ok(CeStreamKind::AuditAnnouncement),
+            145 => Ok(CeStreamKind::JudgmentPublication),
+            _ => Err(NetworkError::InvalidCeStreamKind(value)),
+        }
+    }
+}
+
+impl CeStreamKind {
+    pub fn from_u8(value: u8) -> Result<Self, NetworkError> {
+        Self::try_from(value)
+    }
 }
 
 #[derive(Debug)]
