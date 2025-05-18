55d641f0f82286ef76fb7e92a4f2e1810fe1ab06
Junha Park <0xjunha@gmail.com>
2025-05-18 17:50:06 +0900

    Add `CeStream` trait

--- network/src/streams/ce_streams.rs
+++ network/src/streams/ce_streams.rs
@@ -1,1 +1,33 @@
+use crate::error::NetworkError;
 
+pub enum NodeRole {
+    Node,
+    Builder,
+    Validator,
+    Guarantor,
+    Assurer,
+    Auditor,
+}
+
+pub trait CeStream {
+    const INITIATOR_ROLE: NodeRole;
+    const ACCEPTOR_ROLE: NodeRole;
+
+    fn initiate() -> Result<(), NetworkError>;
+
+    fn respond() -> Result<(), NetworkError>;
+}
+
+pub struct BlockRequest;
+impl CeStream for BlockRequest {
+    const INITIATOR_ROLE: NodeRole = NodeRole::Node;
+    const ACCEPTOR_ROLE: NodeRole = NodeRole::Node;
+
+    fn initiate() -> Result<(), NetworkError> {
+        todo!()
+    }
+
+    fn respond() -> Result<(), NetworkError> {
+        todo!()
+    }
+}
