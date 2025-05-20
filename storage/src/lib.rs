8288f0c14d00df7c5c02f0cef831f017fd320dbe
Junha Park <0xjunha@gmail.com>
2025-05-20 14:43:20 +0900

    Add `NodeStorage` type

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -1,1 +1,27 @@
 //! Storage Interface
+use fr_block::header_db::BlockHeaderDB;
+use fr_state::{error::StateManagerError, manager::StateManager};
+use std::sync::Arc;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum NodeStorageError {
+    #[error("StateManagerError: {0}")]
+    StateManagerError(#[from] StateManagerError),
+}
+
+// TODO: Add storages for Block, Shards, etc.
+pub struct NodeStorage {
+    state_manager: Arc<StateManager>,
+    header_db: Arc<BlockHeaderDB>,
+}
+
+impl NodeStorage {
+    pub fn state_manager(&self) -> Arc<StateManager> {
+        self.state_manager.clone()
+    }
+
+    pub fn header_db(&self) -> Arc<BlockHeaderDB> {
+        self.header_db.clone()
+    }
+}
