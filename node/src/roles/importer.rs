75d68e92e4f4d069e8bb068a4a6809d84a02a240
Junha Park <0xjunha@gmail.com>
2025-05-22 14:42:24 +0900

    Temporarily turn off `BlockImporter::validate_parent_hash`

--- node/src/roles/importer.rs
+++ node/src/roles/importer.rs
@@ -164,7 +164,7 @@ impl BlockImporter {
         block: &Block,
     ) -> Result<(), BlockImportError> {
         let best_header = storage.header_db().get_best_header();
-        Self::validate_parent_hash(&best_header, block)?;
+        // Self::validate_parent_hash(&best_header, block)?; // FIXME: check finality & best header
         Self::validate_timeslot_index(&best_header, block)?;
         // self.validate_prior_state_root()?; // TODO: impl
         Self::validate_xt_hash(block)?;
@@ -214,6 +214,7 @@ impl BlockImporter {
         Ok(())
     }
 
+    #[allow(dead_code)]
     fn validate_parent_hash(
         best_header: &BlockHeader,
         block: &Block,
