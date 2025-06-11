43ce564c7eceba3d173adf07ee8afe5a1e76f1f4
Junha Park <0xjunha@gmail.com>
2025-06-11 16:46:27 +0900

    Reorg stf tests directory

---
+++ conformance-tests/tests/stf/assurances.rs
@@ -0,0 +1,166 @@
+//! Assurances state transition conformance tests
+use async_trait::async_trait;
+use fr_asn_types::types::{assurances::*, common::*};
+use fr_block::types::block::BlockHeader;
+use fr_common::Hash32;
+use fr_conformance_tests::{
+    err_map::assurances::map_error_to_custom_code,
+    generate_typed_tests,
+    harness::{run_test_case, StateTransitionTest},
+};
+use fr_state::{
+    error::StateManagerError,
+    manager::StateManager,
+    types::{ActiveSet, PendingReports, Timeslot},
+};
+use fr_transition::{
+    error::TransitionError,
+    state::{reports::transition_reports_clear_availables, timeslot::transition_timeslot},
+};
+use std::sync::Arc;
+
+struct AssurancesTest;
+
+#[async_trait]
+impl StateTransitionTest for AssurancesTest {
+    const PATH_PREFIX: &'static str = "jamtestvectors-polkajam/stf/assurances/tiny";
+
+    type Input = Input;
+    type JamInput = JamInput;
+    type State = State;
+    type JamTransitionOutput = JamTransitionOutput;
+    type Output = Output;
+    type ErrorCode = AssurancesErrorCode;
+
+    async fn load_pre_state(
+        test_pre_state: &Self::State,
+        state_manager: Arc<StateManager>,
+    ) -> Result<(), StateManagerError> {
+        // Convert ASN pre-state into FastRoll types.
+        let pre_pending_reports = PendingReports::from(test_pre_state.avail_assignments.clone());
+        let pre_active_set = ActiveSet(validators_data_to_validator_set(
+            &test_pre_state.curr_validators,
+        ));
+
+        // Load pre-state info the state cache.
+        state_manager
+            .add_pending_reports(pre_pending_reports)
+            .await?;
+        state_manager.add_active_set(pre_active_set).await?;
+        // Additionally, initialize the timeslot state cache
+        state_manager.add_timeslot(Timeslot::new(0)).await?;
+
+        Ok(())
+    }
+
+    fn convert_input_type(test_input: &Self::Input) -> Result<Self::JamInput, TransitionError> {
+        // Convert ASN Input into FastRoll types.
+        Ok(JamInput {
+            extrinsic: test_input.assurances.clone().into(),
+            timeslot: Timeslot::new(test_input.slot),
+            parent_hash: Hash32::from(test_input.parent),
+        })
+    }
+
+    async fn run_state_transition(
+        state_manager: Arc<StateManager>,
+        _new_header: &mut BlockHeader,
+        jam_input: Self::JamInput,
+    ) -> Result<Self::JamTransitionOutput, TransitionError> {
+        // Run state transitions.
+        transition_timeslot(state_manager.clone(), &jam_input.timeslot).await?;
+
+        let removed_reports = transition_reports_clear_availables(
+            state_manager,
+            &jam_input.extrinsic,
+            jam_input.parent_hash,
+        )
+        .await?;
+
+        Ok(JamTransitionOutput { removed_reports })
+    }
+
+    fn map_error_code(e: TransitionError) -> Self::ErrorCode {
+        map_error_to_custom_code(e)
+    }
+
+    fn extract_output(
+        _new_header: &BlockHeader,
+        transition_output: Option<&Self::JamTransitionOutput>,
+        error_code: &Option<Self::ErrorCode>,
+    ) -> Self::Output {
+        if let Some(error_code) = error_code {
+            return Output::err(error_code.clone());
+        }
+
+        // Convert FastRoll output into ASN Output.
+        Output::ok(transition_output.cloned().unwrap().into())
+    }
+
+    async fn extract_post_state(
+        state_manager: Arc<StateManager>,
+        pre_state: &Self::State,
+        error_code: &Option<Self::ErrorCode>,
+    ) -> Result<Self::State, StateManagerError> {
+        if error_code.is_some() {
+            // Rollback state transition
+            return Ok(pre_state.clone());
+        }
+
+        // Get the posterior state from the state cache.
+        let curr_pending_reports = state_manager.get_pending_reports().await?;
+        let curr_active_set = state_manager.get_active_set().await?;
+
+        Ok(State {
+            avail_assignments: curr_pending_reports.into(),
+            curr_validators: validator_set_to_validators_data(&curr_active_set),
+        })
+    }
+}
+
+generate_typed_tests! {
+    AssurancesTest,
+
+    // Success
+    // Progress with an empty assurances extrinsic.
+    no_assurances_1: "no_assurances-1.json",
+
+    // Success
+    // Several assurances contributing to establishing availability super-majority for some of the cores.
+    some_assurances_1: "some_assurances-1.json",
+
+    // Success
+    // Progress with an empty assurances extrinsic.
+    // Stale work report assignment is removed (but not returned in the output).
+    no_assurances_with_stale_report_1: "no_assurances_with_stale_report-1.json",
+
+    // Fail
+    // One assurance has a bad signature.
+    assurances_with_bad_signature_1: "assurances_with_bad_signature-1.json",
+
+    // TODO: check - this test vector also returns err(not_sorted_or_unique_assurers)
+    // Fail
+    // One assurance has a bad validator index.
+    // assurances_with_bad_validator_index_1: "assurances_with_bad_validator_index-1.json",
+
+    // Fail
+    // One assurance targets a core without any assigned work report.
+    assurance_for_not_engaged_core_1: "assurance_for_not_engaged_core-1.json",
+
+    // Fail
+    // One assurance has a bad attestation parent hash.
+    assurance_with_bad_attestation_parent_1: "assurance_with_bad_attestation_parent-1.json",
+
+    // Fail
+    // One assurance targets a core with a stale report.
+    // We are lenient on the stale report as far as it is available.
+    assurances_for_stale_report_1: "assurances_for_stale_report-1.json",
+
+    // Fail
+    // Assurers not sorted.
+    assurers_not_sorted_or_unique_1: "assurers_not_sorted_or_unique-1.json",
+
+    // Fail
+    // Duplicate assurer.
+    assurers_not_sorted_or_unique_2: "assurers_not_sorted_or_unique-2.json",
+}
---
+++ conformance-tests/tests/stf/authorizations.rs
@@ -0,0 +1,108 @@
+//! Authorizers state transition conformance tests
+use async_trait::async_trait;
+use fr_asn_types::types::authorizations::*;
+use fr_block::types::{block::BlockHeader, extrinsics::guarantees::GuaranteesXt};
+use fr_conformance_tests::{
+    generate_typed_tests,
+    harness::{run_test_case, StateTransitionTest},
+};
+use fr_state::{
+    error::StateManagerError,
+    manager::StateManager,
+    types::{AuthPool, AuthQueue, Timeslot},
+};
+use fr_transition::{error::TransitionError, state::authorizer::transition_auth_pool};
+use std::sync::Arc;
+
+struct AuthorizationsTest;
+
+#[async_trait]
+impl StateTransitionTest for AuthorizationsTest {
+    const PATH_PREFIX: &'static str = "jamtestvectors-polkajam/stf/authorizations/tiny";
+
+    type Input = Input;
+    type JamInput = JamInput;
+    type State = State;
+    type JamTransitionOutput = ();
+    type Output = Output;
+    type ErrorCode = ();
+
+    async fn load_pre_state(
+        test_pre_state: &Self::State,
+        state_manager: Arc<StateManager>,
+    ) -> Result<(), StateManagerError> {
+        // Convert ASN pre-state into FastRoll types.
+        let pre_auth_pool = AuthPool::from(test_pre_state.auth_pools.clone());
+        let pre_auth_queue = AuthQueue::from(test_pre_state.auth_queues.clone());
+
+        // Load pre-state info the state cache.
+        state_manager.add_auth_pool(pre_auth_pool).await?;
+        state_manager.add_auth_queue(pre_auth_queue).await?;
+
+        Ok(())
+    }
+
+    fn convert_input_type(test_input: &Self::Input) -> Result<Self::JamInput, TransitionError> {
+        // Convert ASN Input into FastRoll types.
+        let input_timeslot = Timeslot::new(test_input.slot);
+        let input_extrinsic: GuaranteesXt = test_input.clone().into();
+
+        Ok(JamInput {
+            slot: input_timeslot,
+            extrinsic: input_extrinsic,
+        })
+    }
+
+    async fn run_state_transition(
+        state_manager: Arc<StateManager>,
+        _new_header: &mut BlockHeader,
+        jam_input: Self::JamInput,
+    ) -> Result<Self::JamTransitionOutput, TransitionError> {
+        // Run state transitions.
+        transition_auth_pool(state_manager, &jam_input.extrinsic, jam_input.slot).await
+    }
+
+    fn map_error_code(_e: TransitionError) -> Self::ErrorCode {
+        // No custom error code
+    }
+
+    fn extract_output(
+        _new_header: &BlockHeader,
+        _transition_output: Option<&Self::JamTransitionOutput>,
+        _error_code: &Option<Self::ErrorCode>,
+    ) -> Self::Output {
+        Output
+    }
+
+    async fn extract_post_state(
+        state_manager: Arc<StateManager>,
+        _pre_state: &Self::State,
+        _error_code: &Option<Self::ErrorCode>,
+    ) -> Result<Self::State, StateManagerError> {
+        // Get the posterior state from the state cache.
+        let curr_auth_pool = state_manager.get_auth_pool().await?;
+        let curr_auth_queue = state_manager.get_auth_queue().await?;
+
+        Ok(State {
+            auth_pools: curr_auth_pool.into(),
+            auth_queues: curr_auth_queue.into(),
+        })
+    }
+}
+
+generate_typed_tests! {
+    AuthorizationsTest,
+
+    // No guarantees.
+    // Shift auths left from both pools.
+    progress_authorizations_1: "progress_authorizations-1.json",
+
+    // Guarantees for cores 0 and 1.
+    // Consume authentication from both cores pools.
+    progress_authorizations_2: "progress_authorizations-2.json",
+
+    // Guarantees for core 1.
+    // Shift left authentications for core 0 pool.
+    // Consume authentication for core 1 pool.
+    progress_authorizations_3: "progress_authorizations-3.json",
+}
