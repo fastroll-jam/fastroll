1a0b0170b0878a545891cc93032b8f4069267ac0
Junha Park <0xjunha@gmail.com>
2025-06-11 16:47:28 +0900

    Bump test vectors (#142)
    * Bump test vectors
    
    * Fix test vector paths
    
    * Reorg stf tests directory

---
+++ conformance-tests/tests/stf/statistics.rs
@@ -0,0 +1,138 @@
+//! Statistics state transition conformance tests
+use async_trait::async_trait;
+use fr_asn_types::types::{common::*, statistics::*};
+use fr_block::types::{block::BlockHeader, extrinsics::Extrinsics};
+use fr_conformance_tests::{
+    generate_typed_tests,
+    harness::{run_test_case, StateTransitionTest},
+};
+use fr_pvm_types::stats::{AccumulateStats, OnTransferStats};
+use fr_state::{
+    error::StateManagerError,
+    manager::StateManager,
+    types::{
+        ActiveSet, CoreStats, EpochValidatorStats, OnChainStatistics, ServiceStats, Timeslot,
+        ValidatorStats,
+    },
+};
+use fr_transition::{error::TransitionError, state::statistics::transition_onchain_statistics};
+use std::sync::Arc;
+
+struct StatisticsTest;
+
+#[async_trait]
+impl StateTransitionTest for StatisticsTest {
+    const PATH_PREFIX: &'static str = "jamtestvectors-polkajam/stf/statistics/tiny";
+
+    type Input = Input;
+    type JamInput = JamInput;
+    type State = State;
+    type JamTransitionOutput = ();
+    type Output = Output;
+    type ErrorCode = ();
+
+    async fn load_pre_state(
+        test_pre_state: &Self::State,
+        state_manager: Arc<StateManager>,
+    ) -> Result<(), StateManagerError> {
+        // Convert ASN pre-state into FastRoll types.
+        let pre_onchain_stats = OnChainStatistics {
+            validator_stats: ValidatorStats {
+                curr: EpochValidatorStats::from(test_pre_state.vals_curr_stats.clone()),
+                prev: EpochValidatorStats::from(test_pre_state.vals_last_stats.clone()),
+            },
+            core_stats: CoreStats::default(),
+            service_stats: ServiceStats::default(),
+        };
+
+        let pre_timeslot = Timeslot::new(test_pre_state.slot);
+        let posterior_active_set = ActiveSet(validators_data_to_validator_set(
+            &test_pre_state.curr_validators,
+        ));
+
+        // Load pre-state info the state cache.
+        state_manager
+            .add_onchain_statistics(pre_onchain_stats)
+            .await?;
+        state_manager.add_timeslot(pre_timeslot).await?;
+        state_manager.add_active_set(posterior_active_set).await?;
+
+        Ok(())
+    }
+
+    fn convert_input_type(test_input: &Self::Input) -> Result<Self::JamInput, TransitionError> {
+        // Convert ASN Input into FastRoll types.
+        Ok(JamInput {
+            timeslot: Timeslot::new(test_input.slot),
+            author_index: test_input.author_index,
+            extrinsics: Extrinsics::from(test_input.extrinsic.clone()),
+        })
+    }
+
+    async fn run_state_transition(
+        state_manager: Arc<StateManager>,
+        _new_header: &mut BlockHeader,
+        jam_input: Self::JamInput,
+    ) -> Result<Self::JamTransitionOutput, TransitionError> {
+        // Run state transitions.
+        let pre_timeslot = state_manager.get_timeslot().await?;
+        let next_timeslot = jam_input.timeslot;
+        let epoch_progressed = pre_timeslot.epoch() < next_timeslot.epoch();
+
+        transition_onchain_statistics(
+            state_manager,
+            epoch_progressed,
+            jam_input.author_index,
+            &jam_input.extrinsics,
+            &Vec::new(),
+            AccumulateStats::default(),
+            OnTransferStats::default(),
+        )
+        .await
+    }
+
+    fn map_error_code(_e: TransitionError) -> Self::ErrorCode {
+        // No custom error code
+    }
+
+    fn extract_output(
+        _new_header: &BlockHeader,
+        _transition_output: Option<&Self::JamTransitionOutput>,
+        _error_code: &Option<Self::ErrorCode>,
+    ) -> Self::Output {
+        Output
+    }
+
+    async fn extract_post_state(
+        state_manager: Arc<StateManager>,
+        _pre_state: &Self::State,
+        _error_code: &Option<Self::ErrorCode>,
+    ) -> Result<Self::State, StateManagerError> {
+        // Get the posterior state from the state cache.
+        let curr_onchain_stats = state_manager.get_onchain_statistics().await?;
+        let curr_timeslot = state_manager.get_timeslot().await?;
+        let posterior_active_set = state_manager.get_active_set().await?;
+
+        // Convert FastRoll types post-state into ASN post-state
+        Ok(State {
+            vals_curr_stats: curr_onchain_stats.validator_stats.curr.into(),
+            vals_last_stats: curr_onchain_stats.validator_stats.prev.into(),
+            slot: curr_timeslot.slot(),
+            curr_validators: validator_set_to_validators_data(&posterior_active_set),
+        })
+    }
+}
+
+generate_typed_tests! {
+    StatisticsTest,
+
+    // Empty extrinsic with no epoch change.
+    // Only author blocks counter is incremented.
+    stats_with_empty_extrinsic_1: "stats_with_empty_extrinsic-1.json",
+
+    // Misc extrinsic information with no epoch change.
+    stats_with_epoch_change_1: "stats_with_epoch_change-1.json",
+
+    // Misc extrinsic information with no epoch change.
+    stats_with_some_extrinsic_1: "stats_with_some_extrinsic-1.json",
+}
