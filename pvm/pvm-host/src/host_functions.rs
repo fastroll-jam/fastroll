133ddd736d9e499060d35dbf3ce3e036c18f1e7d
Junha Park <0xjunha@gmail.com>
2025-06-12 22:01:28 +0900

    Revert to v0.6.5: `Octets` account storage keys (post cherry-pick)

--- pvm/pvm-host/src/host_functions.rs
+++ pvm/pvm-host/src/host_functions.rs
@@ -452,10 +452,10 @@ impl HostFunction {
             host_call_panic!()
         }
 
-        let Ok(storage_key) = vm.memory.read_bytes(key_offset, key_size) else {
-            host_call_panic!()
-        };
-        let storage_key = Octets::from_vec(storage_key);
+        // let storage_key = Octets::from_vec(vm.memory.read_bytes(key_offset, key_size)?);
+        let mut key = service_id.encode_fixed(4)?;
+        key.extend(vm.memory.read_bytes(key_offset, key_size)?);
+        let storage_key = hash::<Blake2b256>(&key)?;
 
         let Ok(Some(entry)) = accounts_sandbox
             .get_account_storage_entry(state_manager, service_id, &storage_key)
@@ -520,10 +520,10 @@ impl HostFunction {
             host_call_panic!()
         }
 
-        let Ok(storage_key) = vm.memory.read_bytes(key_offset, key_size) else {
-            host_call_panic!()
-        };
-        let storage_key = Octets::from_vec(storage_key);
+        // let storage_key = Octets::from_vec(vm.memory.read_bytes(key_offset, key_size)?);
+        let mut key = service_id.encode_fixed(4)?;
+        key.extend(vm.memory.read_bytes(key_offset, key_size)?);
+        let storage_key = hash::<Blake2b256>(&key)?;
 
         // Threshold balance change simulation
         let maybe_prev_storage_entry = accounts_sandbox
