abc56f5c9f51012b303fd2be762234fe17ce14cb
Junha Park <0xjunha@gmail.com>
2025-06-22 21:17:34 +0900

    Hotfix: Block header contains winning tickets marker only at the first block after the end of ticket submission period

--- transition/src/procedures/chain_extension.rs
+++ transition/src/procedures/chain_extension.rs
@@ -13,27 +13,29 @@ pub async fn mark_safrole_header_markers(
     state_manager: Arc<StateManager>,
     epoch_progressed: bool,
 ) -> Result<SafroleHeaderMarkers, TransitionError> {
-    let current_timeslot = state_manager.get_timeslot().await?;
-    let current_safrole = state_manager.get_safrole().await?;
+    let prev_timeslot = state_manager.get_timeslot_clean().await?;
+    let curr_timeslot = state_manager.get_timeslot().await?;
+    let curr_safrole = state_manager.get_safrole().await?;
 
     let epoch_marker = if epoch_progressed {
         let prior_entropy = state_manager.get_epoch_entropy_clean().await?;
-        let current_pending_set = current_safrole.pending_set;
+        let curr_pending_set = curr_safrole.pending_set;
         Some(EpochMarker {
             entropy: prior_entropy.current().clone(),
             tickets_entropy: prior_entropy.first_history().clone(),
-            validators: extract_epoch_marker_keys(&current_pending_set),
+            validators: extract_epoch_marker_keys(&curr_pending_set),
         })
     } else {
         None
     };
 
     let needs_winning_tickets_marker = !epoch_progressed
-        && current_timeslot.slot_phase() >= TICKET_CONTEST_DURATION as u32
-        && current_safrole.ticket_accumulator.is_full();
+        && prev_timeslot.slot_phase() < TICKET_CONTEST_DURATION as u32
+        && curr_timeslot.slot_phase() >= TICKET_CONTEST_DURATION as u32
+        && curr_safrole.ticket_accumulator.is_full();
 
     let winning_tickets_marker = if needs_winning_tickets_marker {
-        let marker_vec_outside_in = outside_in_vec(current_safrole.ticket_accumulator.into_vec());
+        let marker_vec_outside_in = outside_in_vec(curr_safrole.ticket_accumulator.into_vec());
         let marker = WinningTicketsMarker::try_from_vec(marker_vec_outside_in)?;
         Some(marker)
     } else {
