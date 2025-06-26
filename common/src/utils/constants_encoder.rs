0ab9678d16d88e712652e3e9beb55c9166396c6a
Junha Park <0xjunha@gmail.com>
2025-06-26 16:02:53 +0900

    [Common] Update chain params in `FETCH`
    Updated chain params to align with GP v0.7.0. Related PR:
    - https://github.com/gavofyork/graypaper/pull/414

--- common/src/utils/constants_encoder.rs
+++ common/src/utils/constants_encoder.rs
@@ -1,18 +1,18 @@
 use crate::{
     ACCUMULATION_GAS_ALL_CORES, ACCUMULATION_GAS_PER_CORE, AUTH_QUEUE_SIZE, BLOCK_HISTORY_LENGTH,
     CORE_COUNT, DATA_SEGMENTS_CHUNKS, EPOCH_LENGTH, ERASURE_CHUNK_SIZE, GUARANTOR_ROTATION_PERIOD,
-    IS_AUTHORIZED_GAS_PER_WORK_PACKAGE, MAX_ACCUMULATE_QUEUE_ENTRIES, MAX_AUTH_POOL_SIZE,
-    MAX_EXPORTS_PER_PACKAGE, MAX_EXTRINSICS_PER_PACKAGE, MAX_IMPORTS_PER_PACKAGE,
-    MAX_IS_AUTHORIZED_CODE_SIZE, MAX_LOOKUP_ANCHOR_AGE, MAX_PACKAGE_AND_DATA_SIZE,
-    MAX_REPORT_DEPENDENCIES, MAX_SERVICE_CODE_SIZE, MAX_WORK_ITEMS_PER_PACKAGE,
+    IS_AUTHORIZED_GAS_PER_WORK_PACKAGE, MAX_AUTH_POOL_SIZE, MAX_EXPORTS_PER_PACKAGE,
+    MAX_EXTRINSICS_PER_PACKAGE, MAX_IMPORTS_PER_PACKAGE, MAX_IS_AUTHORIZED_CODE_SIZE,
+    MAX_LOOKUP_ANCHOR_AGE, MAX_PACKAGE_AND_DATA_SIZE, MAX_REPORT_DEPENDENCIES,
+    MAX_SERVICE_CODE_SIZE, MAX_TICKETS_PER_EXTRINSIC, MAX_WORK_ITEMS_PER_PACKAGE,
     MIN_BALANCE_PER_ITEM, MIN_BALANCE_PER_OCTET, MIN_BASIC_BALANCE, PENDING_REPORT_TIMEOUT,
-    PREIMAGE_EXPIRATION_PERIOD, REFINE_GAS_PER_WORK_PACKAGE, SEGMENT_SIZE, SLOT_DURATION,
+    PREIMAGE_EXPIRATION_PERIOD, REFINE_GAS_PER_WORK_PACKAGE, SLOT_DURATION, TICKETS_PER_VALIDATOR,
     TICKET_CONTEST_DURATION, TRANSFER_MEMO_SIZE, VALIDATOR_COUNT, WORK_REPORT_OUTPUT_SIZE_LIMIT,
 };
 use fr_codec::prelude::*;
 
 pub fn encode_constants_for_fetch_hostcall() -> Result<Vec<u8>, JamCodecError> {
-    let mut buf = Vec::with_capacity(136);
+    let mut buf = Vec::with_capacity(134);
     MIN_BALANCE_PER_ITEM.encode_to_fixed(&mut buf, 8)?; // B_I
     MIN_BALANCE_PER_OCTET.encode_to_fixed(&mut buf, 8)?; // B_L
     MIN_BASIC_BALANCE.encode_to_fixed(&mut buf, 8)?; // B_S
@@ -26,12 +26,13 @@ pub fn encode_constants_for_fetch_hostcall() -> Result<Vec<u8>, JamCodecError> {
     BLOCK_HISTORY_LENGTH.encode_to_fixed(&mut buf, 2)?; // H
     MAX_WORK_ITEMS_PER_PACKAGE.encode_to_fixed(&mut buf, 2)?; // I
     MAX_REPORT_DEPENDENCIES.encode_to_fixed(&mut buf, 2)?; // J
+    MAX_TICKETS_PER_EXTRINSIC.encode_to_fixed(&mut buf, 2)?; // K
     MAX_LOOKUP_ANCHOR_AGE.encode_to_fixed(&mut buf, 4)?; // L
+    TICKETS_PER_VALIDATOR.encode_to_fixed(&mut buf, 2)?; // N
     MAX_AUTH_POOL_SIZE.encode_to_fixed(&mut buf, 2)?; // O
     SLOT_DURATION.encode_to_fixed(&mut buf, 2)?; // P
     AUTH_QUEUE_SIZE.encode_to_fixed(&mut buf, 2)?; // Q
     GUARANTOR_ROTATION_PERIOD.encode_to_fixed(&mut buf, 2)?; // R
-    MAX_ACCUMULATE_QUEUE_ENTRIES.encode_to_fixed(&mut buf, 2)?; // S
     MAX_EXTRINSICS_PER_PACKAGE.encode_to_fixed(&mut buf, 2)?; // T
     PENDING_REPORT_TIMEOUT.encode_to_fixed(&mut buf, 2)?; // U
     VALIDATOR_COUNT.encode_to_fixed(&mut buf, 2)?; // V
@@ -39,7 +40,6 @@ pub fn encode_constants_for_fetch_hostcall() -> Result<Vec<u8>, JamCodecError> {
     MAX_PACKAGE_AND_DATA_SIZE.encode_to_fixed(&mut buf, 4)?; // W_B
     MAX_SERVICE_CODE_SIZE.encode_to_fixed(&mut buf, 4)?; // W_C
     ERASURE_CHUNK_SIZE.encode_to_fixed(&mut buf, 4)?; // W_E
-    SEGMENT_SIZE.encode_to_fixed(&mut buf, 4)?; // W_G
     MAX_IMPORTS_PER_PACKAGE.encode_to_fixed(&mut buf, 4)?; // W_M
     DATA_SEGMENTS_CHUNKS.encode_to_fixed(&mut buf, 4)?; // W_P
     WORK_REPORT_OUTPUT_SIZE_LIMIT.encode_to_fixed(&mut buf, 4)?; // W_R
