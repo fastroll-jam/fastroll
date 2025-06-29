6f7ee7cb042d83fc2c23bab35c2617d8313d379c
Junha Park <0xjunha@gmail.com>
2025-06-29 22:41:19 +0900

    Various improvements for block import testing (#171)
    * Update: fix `ServiceStats` codec: fixed codec for `ServiceId`
    
    * Update: Include state key value to `StateManagerError::StateKeyNotInitialized`
    
    * Update: mitigate overflow in `FORGET`
    
    * Update: better logging in traces tests
    
    * Update: `D` constant value for "tiny" & "small" chain spec

--- common/src/chain_spec/tiny.rs
+++ common/src/chain_spec/tiny.rs
@@ -7,6 +7,13 @@ pub const CORE_COUNT: usize = 2;
 /// `P`: Length of a timeslot in seconds.
 pub const SLOT_DURATION: u64 = 6;
 
+/// `D`: The period in timeslots after which an unreferenced preimage may be expunged.
+/// `PREIMAGE_EXPIRATION_PERIOD` = `MAX_LOOKUP_ANCHOR_AGE` + `8-hour buffer`
+/// 
+/// `D` = (full `PREIMAGE_EXPIRATION_PERIOD`) * (tiny EPOCH_LENGTH) / (full EPOCH_LENGTH) = 384
+/// Note: "tiny" configuration has `D` = 384, but here set as 32 for testing purpose.
+pub const PREIMAGE_EXPIRATION_PERIOD: u32 = 32;
+
 /// `E`: The length of an epoch in timeslots.
 pub const EPOCH_LENGTH: usize = 12;
 
