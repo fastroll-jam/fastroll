31469eec6d9ded9f21dc8c2d21d534afb0c82f67
Junha Park <0xjunha@gmail.com>
2025-07-09 18:51:57 +0900

    [Conformance] Add block import fuzz target server (#179)
    Added block-importing fuzz target server, implementing [fuzz protocol](https://github.com/davxy/jam-stuff/tree/ab225acf809e84673e2c8263e9feafe494eccf67/fuzz-proto) which will support M1 conformance testing.
    
    This PR also includes refactoring that was needed for adding the `fuzz` subcommand to the CLI:
    - Added `cli` root folder (`fastroll` binary crate)
    - Updated `fr-node` crate as a library crate, moving all CLI features to `fastroll` crate
    
    **Note**: The `GetState` fuzz message type is still in development and requires implementing a mechanism to collect all post-state entries after block execution. `SetState`, `ImportBlock` types should be updated accordingly as well - keeping the latest state entries available.
    
    
    * Add fuzzing protocol message types
    
    * (WIP) Add fuzz target CLI subcommand
    
    * Add socket address input validator & fuzz target runner skeleton code
    
    * (WIP) Implement the fuzz target message handlers
    
    * Add a persistent NodeStorage field to `FuzzTargetRunner`
    
    * Add `fastroll` CLI crate and move node binary to it
    
    * Cleanup `fr-node` deps
    
    * Move `fr-fuzz-target` crate under `conformance-tests` folder
    
    * Handle `ImportBlock` message
    
    * Handle `SetState` message and fix `ImportBlock` to return pre state root on block import failure

---
+++ node/src/keystore/dev_account_profile.rs
@@ -0,0 +1,28 @@
+use crate::keystore::load_dev_accounts_from_file;
+use clap::ValueEnum;
+use fr_network::manager::LocalNodeInfo;
+
+#[derive(ValueEnum, Clone, Debug)]
+#[clap(rename_all = "lower")]
+pub enum DevNodeAccountProfile {
+    Alice,
+    Bob,
+    Carol,
+    David,
+    Eve,
+    Fergie,
+}
+
+impl DevNodeAccountProfile {
+    pub fn load_validator_key_info(&self) -> LocalNodeInfo {
+        let devs = load_dev_accounts_from_file();
+        match self {
+            DevNodeAccountProfile::Alice => devs.alice.into(),
+            DevNodeAccountProfile::Bob => devs.bob.into(),
+            DevNodeAccountProfile::Carol => devs.carol.into(),
+            DevNodeAccountProfile::David => devs.david.into(),
+            DevNodeAccountProfile::Eve => devs.eve.into(),
+            DevNodeAccountProfile::Fergie => devs.fergie.into(),
+        }
+    }
+}
