3e73b0bcf64ef32169687bfb4081bef1a05a51b3
Junha Park <0xjunha@gmail.com>
2025-07-09 12:14:01 +0900

    Separate CLI parsing from `init_node`

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,7 +1,19 @@
-use fr_node::{init::init_node, runner::run_node};
+use clap::Parser;
+use fr_common::utils::tracing::setup_tracing;
+use fr_node::{
+    cli::{Cli, CliCommand},
+    init::init_node,
+    runner::run_node,
+};
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    run_node(init_node().await?).await
+    // Config tracing subscriber
+    setup_tracing();
+
+    // CLI args
+    match Cli::parse().command {
+        CliCommand::Run { dev_account } => run_node(init_node(dev_account).await?).await,
+    }
 }
