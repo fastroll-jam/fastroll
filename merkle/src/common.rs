c68abbac38311e7b5161d30e9c67eb9d633e0e7d
Junha Park <0xjunha@gmail.com>
2025-07-25 16:10:13 +0900

    Add tests for `trace` function implementation

--- merkle/src/common.rs
+++ merkle/src/common.rs
@@ -105,9 +105,11 @@ pub(crate) fn trace<H: Hasher, T: MerkleData>(
 }
 
 #[cfg(test)]
-mod tests {
+mod node_tests {
     use super::*;
     use fr_crypto::hash::Blake2b256;
+
+    const PREFIX: &[u8] = b"node";
 
     #[test]
     fn test_node_empty() -> Result<(), MerkleError> {
@@ -133,7 +135,7 @@ mod tests {
         let root = node::<Blake2b256, Vec<u8>>(data)?;
 
         let expected =
-            hash::<Blake2b256>(&[b"node".to_vec(), vec![10, 11], vec![12, 13]].concat())?.to_vec();
+            hash::<Blake2b256>(&[PREFIX.to_vec(), vec![10, 11], vec![12, 13]].concat())?.to_vec();
 
         assert_eq!(root, expected);
         Ok(())
@@ -145,9 +147,9 @@ mod tests {
         let root = node::<Blake2b256, Vec<u8>>(data)?;
 
         let hash_10111213 =
-            hash::<Blake2b256>(&[b"node".to_vec(), vec![10, 11], vec![12, 13]].concat())?.to_vec();
+            hash::<Blake2b256>(&[PREFIX.to_vec(), vec![10, 11], vec![12, 13]].concat())?.to_vec();
         let expected =
-            hash::<Blake2b256>(&[b"node".to_vec(), hash_10111213, vec![14, 15]].concat())?.to_vec();
+            hash::<Blake2b256>(&[PREFIX.to_vec(), hash_10111213, vec![14, 15]].concat())?.to_vec();
 
         assert_eq!(root, expected);
         Ok(())
@@ -159,15 +161,182 @@ mod tests {
         let root = node::<Blake2b256, Vec<u8>>(data)?;
 
         let hash_0123 =
-            hash::<Blake2b256>(&[b"node".to_vec(), vec![0, 1], vec![2, 3]].concat())?.to_vec();
+            hash::<Blake2b256>(&[PREFIX.to_vec(), vec![0, 1], vec![2, 3]].concat())?.to_vec();
         let hash_012345 =
-            hash::<Blake2b256>(&[b"node".to_vec(), hash_0123, vec![4, 5]].concat())?.to_vec();
+            hash::<Blake2b256>(&[PREFIX.to_vec(), hash_0123, vec![4, 5]].concat())?.to_vec();
         let hash_6789 =
-            hash::<Blake2b256>(&[b"node".to_vec(), vec![6, 7], vec![8, 9]].concat())?.to_vec();
+            hash::<Blake2b256>(&[PREFIX.to_vec(), vec![6, 7], vec![8, 9]].concat())?.to_vec();
         let expected =
-            hash::<Blake2b256>(&[b"node".to_vec(), hash_012345, hash_6789].concat())?.to_vec();
+            hash::<Blake2b256>(&[PREFIX.to_vec(), hash_012345, hash_6789].concat())?.to_vec();
 
         assert_eq!(root, expected);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod trace_tests {
+    use super::*;
+    use fr_crypto::hash::Blake2b256;
+
+    const PREFIX: &[u8] = b"node";
+
+    #[test]
+    fn test_trace_empty_data() -> Result<(), MerkleError> {
+        let data: &[Vec<u8>] = &[];
+        let trace_result = trace::<Blake2b256, Vec<u8>>(data, 0)?;
+        assert_eq!(trace_result, Vec::<Vec<u8>>::new());
+        Ok(())
+    }
+
+    #[test]
+    fn test_trace_single_item() -> Result<(), MerkleError> {
+        let data: &[Vec<u8>] = &[vec![0]];
+        let trace_result = trace::<Blake2b256, Vec<u8>>(data, 0)?;
+        assert_eq!(trace_result, Vec::<Vec<u8>>::new());
+        Ok(())
+    }
+
+    #[test]
+    fn test_trace_two_items() -> Result<(), MerkleError> {
+        let data: &[Vec<u8>] = &[vec![0, 1], vec![2, 3]];
+
+        let trace_0 = trace::<Blake2b256, Vec<u8>>(data, 0)?;
+        assert_eq!(trace_0.len(), 1);
+        assert_eq!(trace_0[0], vec![2, 3]);
+
+        let trace_1 = trace::<Blake2b256, Vec<u8>>(data, 1)?;
+        assert_eq!(trace_1.len(), 1);
+        assert_eq!(trace_1[0], vec![0, 1]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_trace_three_items() -> Result<(), MerkleError> {
+        let data: &[Vec<u8>] = &[vec![0, 1], vec![2, 3], vec![4, 5]];
+
+        let trace_0 = trace::<Blake2b256, Vec<u8>>(data, 0)?;
+        assert_eq!(trace_0.len(), 2);
+        assert_eq!(trace_0[0], vec![4, 5]);
+        assert_eq!(trace_0[1], vec![2, 3]);
+
+        let trace_1 = trace::<Blake2b256, Vec<u8>>(data, 1)?;
+        assert_eq!(trace_1.len(), 2);
+        assert_eq!(trace_1[0], vec![4, 5]);
+        assert_eq!(trace_1[1], vec![0, 1]);
+
+        let trace_2 = trace::<Blake2b256, Vec<u8>>(data, 2)?;
+        assert_eq!(trace_2.len(), 1);
+        let expected_sibling =
+            hash::<Blake2b256>(&[PREFIX.to_vec(), vec![0, 1], vec![2, 3]].concat())?.to_vec();
+        assert_eq!(trace_2[0], expected_sibling);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_trace_four_items() -> Result<(), MerkleError> {
+        let data: &[Vec<u8>] = &[vec![0, 1], vec![2, 3], vec![4, 5], vec![6, 7]];
+
+        let trace_0 = trace::<Blake2b256, Vec<u8>>(data, 0)?;
+        assert_eq!(trace_0.len(), 2);
+        let expected_right_subtree =
+            hash::<Blake2b256>(&[PREFIX.to_vec(), vec![4, 5], vec![6, 7]].concat())?.to_vec();
+        assert_eq!(trace_0[0], expected_right_subtree);
+        assert_eq!(trace_0[1], vec![2, 3]);
+
+        let trace_2 = trace::<Blake2b256, Vec<u8>>(data, 2)?;
+        assert_eq!(trace_2.len(), 2);
+        let expected_left_subtree =
+            hash::<Blake2b256>(&[PREFIX.to_vec(), vec![0, 1], vec![2, 3]].concat())?.to_vec();
+        assert_eq!(trace_2[0], expected_left_subtree);
+        assert_eq!(trace_2[1], vec![6, 7]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_trace_five_items() -> Result<(), MerkleError> {
+        let data: &[Vec<u8>] = &[vec![0, 1], vec![2, 3], vec![4, 5], vec![6, 7], vec![8, 9]];
+
+        let trace_0 = trace::<Blake2b256, Vec<u8>>(data, 0)?;
+        assert_eq!(trace_0.len(), 3);
+        let expected_right_subtree =
+            hash::<Blake2b256>(&[PREFIX.to_vec(), vec![6, 7], vec![8, 9]].concat())?.to_vec();
+        assert_eq!(trace_0[0], expected_right_subtree);
+        assert_eq!(trace_0[1], vec![4, 5]);
+        assert_eq!(trace_0[2], vec![2, 3]);
+
+        let trace_4 = trace::<Blake2b256, Vec<u8>>(data, 4)?;
+        assert_eq!(trace_4.len(), 2);
+        let hash_0123 =
+            hash::<Blake2b256>(&[PREFIX.to_vec(), vec![0, 1], vec![2, 3]].concat())?.to_vec();
+        let expected_left_subtree =
+            hash::<Blake2b256>(&[PREFIX.to_vec(), hash_0123, vec![4, 5]].concat())?.to_vec();
+        assert_eq!(trace_4[0], expected_left_subtree);
+        assert_eq!(trace_4[1], vec![6, 7]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_trace_verify_two_items() -> Result<(), MerkleError> {
+        let data: &[Vec<u8>] = &[vec![0, 1], vec![2, 3]];
+        let root = node::<Blake2b256, Vec<u8>>(data)?;
+
+        let trace_0 = trace::<Blake2b256, Vec<u8>>(data, 0)?;
+        let leaf = &data[0];
+
+        let reconstructed_root =
+            hash::<Blake2b256>(&[PREFIX.to_vec(), leaf.clone(), trace_0[0].clone()].concat())?
+                .to_vec();
+
+        assert_eq!(reconstructed_root, root);
+        Ok(())
+    }
+
+    #[test]
+    fn test_trace_verify_four_items() -> Result<(), MerkleError> {
+        let data: &[Vec<u8>] = &[vec![0, 1], vec![2, 3], vec![4, 5], vec![6, 7]];
+        let root = node::<Blake2b256, Vec<u8>>(data)?;
+
+        let trace_2 = trace::<Blake2b256, Vec<u8>>(data, 2)?;
+        let leaf = &data[2];
+
+        let level1 =
+            hash::<Blake2b256>(&[PREFIX.to_vec(), leaf.clone(), trace_2[1].clone()].concat())?
+                .to_vec();
+
+        let reconstructed_root =
+            hash::<Blake2b256>(&[PREFIX.to_vec(), trace_2[0].clone(), level1].concat())?.to_vec();
+
+        assert_eq!(reconstructed_root, root);
+        Ok(())
+    }
+
+    #[test]
+    fn test_trace_hash_input_three_items() -> Result<(), MerkleError> {
+        // Test trace function with Hash32 type inputs
+        let hash1 = hash::<Blake2b256>(b"012")?;
+        let hash2 = hash::<Blake2b256>(b"345")?;
+        let hash3 = hash::<Blake2b256>(b"678")?;
+
+        let data: &[Hash32] = &[hash1.clone(), hash2.clone(), hash3.clone()];
+
+        let trace_0 = trace::<Blake2b256, Hash32>(data, 0)?;
+        assert_eq!(trace_0.len(), 2);
+        assert_eq!(trace_0[0], hash3);
+        assert_eq!(trace_0[1], hash2);
+
+        let trace_1 = trace::<Blake2b256, Hash32>(data, 1)?;
+        assert_eq!(trace_1.len(), 2);
+        assert_eq!(trace_1[0], hash3);
+        assert_eq!(trace_1[1], hash1);
+
+        let trace_2 = trace::<Blake2b256, Hash32>(data, 2)?;
+        assert_eq!(trace_2.len(), 1);
+        let sibling_expected =
+            hash::<Blake2b256>(&[PREFIX, hash1.as_slice(), hash2.as_slice()].concat())?;
+        assert_eq!(trace_2[0], sibling_expected);
+
         Ok(())
     }
 }
