217f627d92ac0b8c9ef6ae7c0cbe615d77d2dec0
Junha Park <0xjunha@gmail.com>
2025-07-30 21:11:12 +0900

    (WIP) `LOOKUP` hostcall tests

--- pvm/pvm-host/src/host_functions/test_utils.rs
+++ pvm/pvm-host/src/host_functions/test_utils.rs
@@ -1,12 +1,58 @@
-use fr_common::SignedGas;
-use fr_pvm_core::state::{memory::Memory, register::Register, vm_state::VMState};
-use fr_pvm_types::constants::REGISTERS_COUNT;
+use crate::context::{AccumulateHostContext, AccumulateHostContextPair};
+use fr_common::{ServiceId, SignedGas};
+use fr_pvm_core::state::{
+    memory::{AccessType, Memory},
+    register::Register,
+    vm_state::VMState,
+};
+use fr_pvm_types::{
+    common::{MemAddress, RegValue},
+    constants::{MEMORY_SIZE, PAGE_SIZE, REGISTERS_COUNT},
+};
+use std::{error::Error, ops::Range};
 
-pub(crate) fn create_vm_state(gas_counter: SignedGas) -> VMState {
+pub(crate) fn mock_empty_vm_state(gas_counter: SignedGas) -> VMState {
     VMState {
         regs: [Register::default(); REGISTERS_COUNT],
         memory: Memory::default(),
         pc: 0,
         gas_counter,
     }
+}
+
+pub(crate) fn mock_vm_state(
+    gas_counter: SignedGas,
+    pc: RegValue,
+    regs: [Register; REGISTERS_COUNT],
+    memory: Memory,
+) -> VMState {
+    VMState {
+        regs,
+        memory,
+        pc,
+        gas_counter,
+    }
+}
+
+pub(crate) fn mock_accumulate_host_context(
+    accumulate_host: ServiceId,
+) -> AccumulateHostContextPair {
+    let context = AccumulateHostContext {
+        accumulate_host,
+        ..Default::default()
+    };
+    AccumulateHostContextPair {
+        x: Box::new(context.clone()),
+        y: Box::new(context),
+    }
+}
+
+pub(crate) fn mock_memory(
+    readable_range: Range<MemAddress>,
+    writable_range: Range<MemAddress>,
+) -> Result<Memory, Box<dyn Error>> {
+    let mut mem = Memory::new(MEMORY_SIZE, PAGE_SIZE);
+    mem.set_address_range_access(readable_range, AccessType::ReadOnly)?;
+    mem.set_address_range_access(writable_range, AccessType::ReadWrite)?;
+    Ok(mem)
 }
