af10d87f32925a05685ddf730ab39aa77218eab8
Junha Park <0xjunha@gmail.com>
2025-08-05 12:10:43 +0900

    [Update] INFO host call: Do not skip regs

--- pvm/pvm-host/src/host_functions.rs
+++ pvm/pvm-host/src/host_functions.rs
@@ -643,8 +643,9 @@ impl HostFunction {
         // Encode account metadata with JAM Codec
         let info = metadata.encode_for_info_hostcall()?;
 
+        // TODO: check - GP using 11 & 12 for f & l
         // f
-        let info_read_offset = match vm.regs[11].as_usize() {
+        let info_read_offset = match vm.regs[9].as_usize() {
             Ok(info_read_offset_reg) => info_read_offset_reg.min(info.len()),
             Err(_) => info.len(),
         };
@@ -654,7 +655,7 @@ impl HostFunction {
             .expect("info_read_offset is less than info blob length");
 
         // l
-        let info_write_len = match vm.regs[12].as_usize() {
+        let info_write_len = match vm.regs[10].as_usize() {
             Ok(info_write_len_reg) => info_write_len_reg.min(info_blob_len_minus_offset),
             Err(_) => info_blob_len_minus_offset,
         };
