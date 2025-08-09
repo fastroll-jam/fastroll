12d86bd0e9fad30690a2b6e66c6e187a59bbb4c5
Junha Park <0xjunha@gmail.com>
2025-08-09 20:55:30 +0900

    [Node] Remove upfront Xt validations in block import

--- node/src/roles/importer.rs
+++ node/src/roles/importer.rs
@@ -143,9 +143,6 @@ impl BlockImporter {
             return Ok((post_state_root, account_state_changes));
         }
 
-        // Validate Xts
-        Self::validate_xts(&storage, &block).await?; // TODO: consider removing upfront validation
-
         // Validate header fields (prior to STF)
         Self::validate_block_header_prior_stf(&storage, &block).await?;
         // Re-execute STF
@@ -160,6 +157,7 @@ impl BlockImporter {
 
     /// Note: Currently, each STF validates Xt types as well. Up-front Xt validations might work
     /// incorrectly, since some validation rules need to refer to partially transitioned state.
+    #[allow(dead_code)]
     async fn validate_xts(storage: &NodeStorage, block: &Block) -> Result<(), BlockImportError> {
         let prior_timeslot = storage.state_manager().get_timeslot_clean().await?;
         let curr_timeslot_index = block.header.timeslot_index();
