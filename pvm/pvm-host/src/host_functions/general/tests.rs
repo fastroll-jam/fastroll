aa2c21ae25fe390c1023ce93ecbc00cc8a0b732c
Junha Park <0xjunha@gmail.com>
2025-08-12 21:53:39 +0900

    Add `FETCH` test fixture

--- pvm/pvm-host/src/host_functions/general/tests.rs
+++ pvm/pvm-host/src/host_functions/general/tests.rs
@@ -7,14 +7,17 @@ use crate::{
     },
 };
 use fr_common::{
-    utils::tracing::setup_tracing, Balance, ByteEncodable, Octets, PreimagesKey, ServiceId,
-    SignedGas, StorageKey, HASH_SIZE,
+    utils::tracing::setup_tracing,
+    workloads::{RefinementContext, WorkItems, WorkPackage},
+    Balance, ByteEncodable, CodeHash, EntropyHash, Octets, PreimagesKey, ServiceId, SignedGas,
+    StorageKey, HASH_SIZE,
 };
 use fr_pvm_core::state::state_change::{HostCallVMStateChange, MemWrite};
 use fr_pvm_types::{
-    common::{MemAddress, RegValue},
+    common::{MemAddress, RegValue, WorkPackageImportSegments},
     constants::{HOSTCALL_BASE_GAS_CHARGE, PAGE_SIZE},
     exit_reason::ExitReason,
+    invoke_args::AccumulateInputs,
 };
 use fr_state::types::{AccountPreimagesEntry, AccountStorageEntry};
 use std::{error::Error, ops::Range, sync::Arc};
@@ -57,6 +60,342 @@ mod gas_tests {
         };
         assert_eq!(res, expected);
         Ok(())
+    }
+}
+
+#[allow(dead_code)]
+mod fetch_tests {
+    use super::*;
+    use crate::context::{
+        partial_state::AccumulatePartialState, AccumulateHostContext, AccumulateHostContextPair,
+        IsAuthorizedHostContext, RefineHostContext,
+    };
+    use fr_common::{
+        constants_encoder::encode_constants_for_fetch_hostcall,
+        workloads::{WorkExecutionResult, WorkItem},
+        AuthHash, BeefyRoot, BlockHeaderHash, Hash32, SegmentRoot, StateRoot, WorkPackageHash,
+        SEGMENT_SIZE,
+    };
+    use fr_pvm_types::{
+        common::ExportDataSegment,
+        invoke_args::{
+            AccumulateInvokeArgs, AccumulateOperand, DeferredTransfer, IsAuthorizedInvokeArgs,
+            RefineInvokeArgs, TransferMemo,
+        },
+    };
+    use std::collections::{BTreeSet, HashMap, HashSet};
+
+    struct RegParams {
+        mem_write_offset: MemAddress,
+        fetch_offset: usize,
+        fetch_length: usize,
+        data_id: usize,
+        index_reg_11: usize,
+        index_reg_12: usize,
+    }
+
+    impl Default for RegParams {
+        fn default() -> Self {
+            Self {
+                mem_write_offset: PAGE_SIZE as MemAddress,
+                fetch_offset: 5,
+                fetch_length: 10,
+                data_id: 0,
+                index_reg_11: 0,
+                index_reg_12: 0,
+            }
+        }
+    }
+
+    struct FetchTestFixture {
+        regs: RegParams,
+        chain_params_encoded: Vec<u8>,
+        entropy: EntropyHash,
+        auth_trace: Vec<u8>,
+        extrinsics: Vec<Vec<Vec<u8>>>,
+        imports: WorkPackageImportSegments,
+        package: WorkPackage,
+        accumulate_inputs: AccumulateInputs,
+    }
+
+    impl Default for FetchTestFixture {
+        fn default() -> Self {
+            let imports_segment = ExportDataSegment::try_from(vec![3; SEGMENT_SIZE]).unwrap();
+            let package_auth_code_hash = CodeHash::from_hex("0x2").unwrap();
+            let package_config_blob = vec![4; 100];
+            let package_auth_token = vec![5; 100];
+            let package_refine_context = RefinementContext {
+                anchor_header_hash: BlockHeaderHash::from_hex("0x3").unwrap(),
+                anchor_state_root: StateRoot::from_hex("0x4").unwrap(),
+                anchor_beefy_root: BeefyRoot::from_hex("0x5").unwrap(),
+                lookup_anchor_header_hash: BlockHeaderHash::from_hex("0x6").unwrap(),
+                lookup_anchor_timeslot: 10,
+                prerequisite_work_packages: BTreeSet::new(),
+            };
+            let work_items_in_package = 3;
+            let work_items = WorkItems::try_from(vec![
+                WorkItem {
+                    service_id: 0,
+                    payload_blob: Octets::from_vec(vec![7; 100]),
+                    ..Default::default()
+                },
+                WorkItem {
+                    service_id: 1,
+                    payload_blob: Octets::from_vec(vec![8; 100]),
+                    ..Default::default()
+                },
+                WorkItem {
+                    service_id: 2,
+                    payload_blob: Octets::from_vec(vec![9; 100]),
+                    ..Default::default()
+                },
+            ])
+            .unwrap();
+            let package = WorkPackage {
+                authorizer_service_id: ServiceId::MAX,
+                auth_code_hash: package_auth_code_hash,
+                context: package_refine_context,
+                auth_token: Octets::from_vec(package_auth_token),
+                config_blob: Octets::from_vec(package_config_blob),
+                work_items,
+            };
+
+            let deferred_transfers = vec![DeferredTransfer {
+                from: 0,
+                to: 1,
+                amount: 333,
+                memo: TransferMemo::from_hex("0xabc").unwrap(),
+                gas_limit: 10,
+            }];
+            let accumulate_operands = vec![AccumulateOperand {
+                work_package_hash: WorkPackageHash::from_hex("0x7").unwrap(),
+                segment_root: SegmentRoot::default(),
+                authorizer_hash: AuthHash::default(),
+                work_item_payload_hash: Hash32::default(),
+                accumulate_gas_limit: 100,
+                refine_result: WorkExecutionResult::Output(Octets::from_vec(vec![])),
+                auth_trace: vec![],
+            }];
+            Self {
+                regs: RegParams::default(),
+                chain_params_encoded: encode_constants_for_fetch_hostcall().unwrap(),
+                entropy: EntropyHash::from_hex("0x1").unwrap(),
+                auth_trace: vec![0; 100],
+                extrinsics: vec![vec![vec![1; 100]; 64]; work_items_in_package], // 64 extrinsics per work-item / 3 work-items // TODO: distinct xts
+                imports: vec![vec![imports_segment; 1024]; work_items_in_package], // 1024 imports per work-item / 3 work-items
+                package,
+                accumulate_inputs: AccumulateInputs::new(deferred_transfers, accumulate_operands),
+            }
+        }
+    }
+
+    impl FetchTestFixture {
+        fn prepare_vm_builder(&self) -> Result<VMStateBuilder, Box<dyn Error>> {
+            Ok(VMStateBuilder::builder()
+                .with_pc(0)
+                .with_gas_counter(100)
+                .with_reg(7, self.regs.mem_write_offset)
+                .with_reg(8, self.regs.fetch_offset as RegValue)
+                .with_reg(9, self.regs.fetch_length as RegValue)
+                .with_reg(10, self.regs.data_id as RegValue)
+                .with_reg(11, self.regs.index_reg_11 as RegValue)
+                .with_reg(12, self.regs.index_reg_12 as RegValue))
+        }
+
+        fn prepare_is_authorized_invocation_context(
+            &self,
+        ) -> Result<InvocationContext<MockStateManager>, Box<dyn Error>> {
+            Ok(InvocationContext::X_I(IsAuthorizedHostContext {
+                invoke_args: IsAuthorizedInvokeArgs {
+                    package: self.package.clone(),
+                    core_idx: 1,
+                },
+            }))
+        }
+
+        fn prepare_refine_invocation_context(
+            &self,
+        ) -> Result<InvocationContext<MockStateManager>, Box<dyn Error>> {
+            Ok(InvocationContext::X_R(RefineHostContext {
+                next_instance_id: 0,
+                pvm_instances: HashMap::new(),
+                export_segments: Vec::new(),
+                refine_entropy: self.entropy.clone(),
+                invoke_args: RefineInvokeArgs {
+                    core_idx: 1,
+                    item_idx: 2,
+                    package: self.package.clone(),
+                    auth_trace: self.auth_trace.clone(),
+                    import_segments: self.imports.clone(),
+                    export_segments_offset: 0,
+                    extrinsic_data_map: HashMap::new(), // TODO: fill data
+                },
+            }))
+        }
+
+        fn prepare_is_accumulate_invocation_context(
+            &self,
+        ) -> Result<InvocationContext<MockStateManager>, Box<dyn Error>> {
+            let context = AccumulateHostContext {
+                accumulate_host: 0,
+                partial_state: AccumulatePartialState::default(),
+                next_new_service_id: 0,
+                deferred_transfers: vec![],
+                yielded_accumulate_hash: None,
+                provided_preimages: HashSet::new(),
+                invoke_args: AccumulateInvokeArgs {
+                    inputs: self.accumulate_inputs.clone(),
+                    ..Default::default()
+                },
+                curr_entropy: self.entropy.clone(),
+            };
+            Ok(InvocationContext::X_A(AccumulateHostContextPair {
+                x: Box::new(context.clone()),
+                y: Box::new(context),
+            }))
+        }
+    }
+
+    mod is_authorized {
+        use super::*;
+
+        #[tokio::test]
+        async fn test_fetch_is_authorized_id_0() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_is_authorized_id_7() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_is_authorized_id_8() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_is_authorized_id_9() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_is_authorized_id_10() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_is_authorized_id_11() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_is_authorized_id_12() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_is_authorized_id_13() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+    }
+
+    mod refine {
+        use super::*;
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_0() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_1() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_2() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_3() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_4() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_5() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_6() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_7() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_8() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_9() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_10() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_11() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_12() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_refine_id_13() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+    }
+
+    mod accumulate {
+        use super::*;
+
+        #[tokio::test]
+        async fn test_fetch_accumulate_id_0() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_accumulate_id_1() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_accumulate_id_14() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn test_fetch_accumulate_id_15() -> Result<(), Box<dyn Error>> {
+            Ok(())
+        }
     }
 }
 
