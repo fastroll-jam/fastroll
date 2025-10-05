45583ee67b62515a02ddbc7d928522c00c88946c
Junha Park <0xjunha@gmail.com>
2025-10-05 22:31:58 +0900

    Bump test vector and uncomment block import tests

--- integration/tests/block_importer.rs
+++ integration/tests/block_importer.rs
@@ -1,5 +1,5 @@
 //! Block importer integration tests
 #[cfg(feature = "tiny")]
 mod block_importer_all {
-    // include!(concat!(env!("OUT_DIR"), "/generated_block_import_tests.rs"));
+    include!(concat!(env!("OUT_DIR"), "/generated_block_import_tests.rs"));
 }
