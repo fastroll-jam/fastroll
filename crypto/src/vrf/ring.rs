91e57244ea4b39e478c9897ccd62c08d97b88f47
Junha Park <0xjunha@gmail.com>
2026-01-08 20:56:33 +0900

    [Crypto] Use unchecked `ark-vrf` deserialization (#347)

--- crypto/src/vrf/ring.rs
+++ crypto/src/vrf/ring.rs
@@ -1,6 +1,6 @@
 use crate::{error::CryptoError, types::*};
-use ark_vrf::{codec::point_decode, suites::bandersnatch};
-use bandersnatch::{BandersnatchSha512Ell2, Public, RingProofParams};
+use ark_vrf::{reexports::ark_serialize::CanonicalDeserialize, suites::bandersnatch};
+use bandersnatch::{Public, RingProofParams};
 use fr_common::ByteEncodable;
 use tracing::instrument;
 
@@ -11,14 +11,10 @@ pub(crate) fn validator_set_to_bandersnatch_ring(
 ) -> Result<Vec<Public>, CryptoError> {
     let mut public_keys = vec![];
     validator_set.iter().for_each(|validator_key| {
-        match point_decode::<BandersnatchSha512Ell2>(validator_key.bandersnatch.as_slice()) {
-            Ok(decoded_point) => {
-                public_keys.push(Public::from(decoded_point));
-            }
-            Err(_) => {
-                public_keys.push(Public::from(RingProofParams::padding_point()));
-            } // Use the padding point if the decoding fails.
-        };
+        // Use the padding point if the decoding fails.
+        let pk = Public::deserialize_compressed_unchecked(validator_key.bandersnatch.as_slice())
+            .unwrap_or(Public::from(RingProofParams::padding_point()));
+        public_keys.push(pk);
     });
     Ok(public_keys)
 }
