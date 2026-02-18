daa8f92c8729924ea9705dbbc1c0e4e7c25b578c
Junha Park <0xjunha@gmail.com>
2026-02-18 21:17:05 +0900

    Revert #371

--- block/src/types/extrinsics/tickets.rs
+++ block/src/types/extrinsics/tickets.rs
@@ -30,36 +30,12 @@ impl Display for TicketsXt {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, JamEncode, JamDecode)]
 pub struct TicketsXtEntry {
     /// `e`: The ticket entry index, either 0 or 1.
     pub entry_index: u8,
     /// `p`: The ticket identifier (note: different from `Ticket` which contains hash of the proof as a ticket id)
     pub ticket_proof: BandersnatchRingVrfSig,
-}
-
-impl JamEncode for TicketsXtEntry {
-    fn size_hint(&self) -> usize {
-        self.entry_index.size_hint() + self.ticket_proof.size_hint()
-    }
-
-    fn encode_to<T: JamOutput>(&self, dest: &mut T) -> Result<(), JamCodecError> {
-        self.entry_index.encode_to_fixed(dest, 1)?;
-        self.ticket_proof.encode_to(dest)?;
-        Ok(())
-    }
-}
-
-impl JamDecode for TicketsXtEntry {
-    fn decode<I: JamInput>(input: &mut I) -> Result<Self, JamCodecError>
-    where
-        Self: Sized,
-    {
-        Ok(Self {
-            entry_index: u8::decode_fixed(input, 1)?,
-            ticket_proof: BandersnatchRingVrfSig::decode(input)?,
-        })
-    }
 }
 
 impl Display for TicketsXtEntry {
